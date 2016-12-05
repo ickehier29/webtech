eecc4c232a990a18b9346550e3b335889ba255f5
Peter <p.hahne@campus.tu-berlin.de>
2016-12-05 23:00:03 +0000

    Homework 3 Part 2 till Lists

--- homework3.js
+++ homework3.js
@@ -28,3 +28,55 @@ function countBs(s)
 {
   return countChar(s, "B");
 }
+
+
+function range(start, end, step) {
+  var result = [];
+  if(step == 0) return result;
+  if(arguments.length < 3)
+    step = 1;
+  if(step > 0) {
+    if(start > end) {
+     var temp = start;
+      start = end;
+	  end = temp;
+    }
+  	for(var c = start; c <= end; c+=step)
+    	result.push(c);
+  	return result;
+  }
+  if(start < end) {
+     var temp = start;
+      start = end;
+	  end = temp;
+  }
+  for(var c = start; c >= end; c+=step)
+    result.push(c);
+  return result;
+}
+
+function sum(array) {
+  var result = 0;
+  for(var c = 0; c < array.length; c++)
+    result += array[c];
+  return result;
+}
+
+function reverseArray(input) {
+  var result = [];
+  var length = input.length;
+  for(var c = 1; c <= length; c++)   
+    result.push(input[length-c]);
+  return result;
+}
+
+function reverseArrayInPlace(input) {
+  var temp;
+  var length = input.length;
+  for(var c = 0; c < Math.floor(length/2); c++) {
+    temp =  input[c];
+    input[c] = input[length-c-1];
+    input[length-c-1] = temp;
+  }
+  return input;
+}
